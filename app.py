bbed019b006a9319a3f77095e8e8f9d50b3b6d3a
Gabriele <gabriele.caletti@outlook.it>
2023-12-24 17:40:39 +0100

    new file examples

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from utils.cv_booster import cvBooster
 
 if __name__=='__main__':
     cv = cvBooster("./test/GabrieleCaletti.pdf")
-    print(cv.infos)
-    cv.re_write(cv.education, "expand")
+    print(cv.text)
+    #cv.re_write(cv.education, "expand")
